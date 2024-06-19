3d51ac496e8f88a792eb78ad6734094db6da5db9
Jake Ratcliffe <JakeR1342@gmail.com>
2024-06-19 21:14:28 +0100

    Updated database client creation

--- Home.py
+++ Home.py
@@ -1,26 +1,18 @@
-import logging
-import os
-import requests
-import streamlit as st
 from pathlib import Path
+
+import streamlit as st
 from PIL import Image
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from streamlit_lottie import st_lottie
 
 # Set the page title and icon and set layout to "wide" to minimise margains
 st.set_page_config(page_title="Claan ChAAos", page_icon=":dragon:")
 
 
 if "home_db" not in st.session_state:
-    mongo_user = st.secrets["MONGO_USER"]
-    mongo_pass = st.secrets["MONGO_PASS"]
-    uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@claanapp.l2vlfwo.mongodb.net/?retryWrites=true&w=majority"
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
+    client: MongoClient = MongoClient(**st.secrets["mongo"])
     # Send a ping to confirm a successful connection
     try:
-        client.admin.command('ping')
+        client.admin.command("ping")
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
@@ -43,7 +35,7 @@ def main():
     # Header section
     with st.container():
         # Create columns
-        head_l, head_r = st.columns((2.5,1))
+        head_l, head_r = st.columns((2.5, 1))
 
         with head_l:
             # Add a subheader
@@ -56,8 +48,12 @@ def main():
             st.image(logo_img)
 
         # Add description
-        st.write("Welcome to seasion 4 of Claans at Advancing Analytics. This time around things are taking a more relaxed turn but retaining a healthy dose of that Claans flair!")
-        st.write("Using the Claan Portal you can see the scores as they stand, see this fortnights quests, and access the Claan area log quests, steps and activities!")
+        st.write(
+            "Welcome to seasion 4 of Claans at Advancing Analytics. This time around things are taking a more relaxed turn but retaining a healthy dose of that Claans flair!"
+        )
+        st.write(
+            "Using the Claan Portal you can see the scores as they stand, see this fortnights quests, and access the Claan area log quests, steps and activities!"
+        )
 
         # Add spacer
         st.write("---")
@@ -71,44 +67,58 @@ def main():
         scores = [i for i in st.session_state.home_col.find()]
 
         # Create column for each claan
-        col1, col2, col3, col4 = st.columns((1,1,1,1))
+        col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
 
         # Add content to first column
         with col1:
             # Add the claan image
             st.image(earth_img)
             # Get scores for the claan, using the sum of the scores, and the last entry as the delta
-            earth_scores = [i['Score'] for i in scores if (i['Claan']=="Earth Striders")]
+            earth_scores = [
+                i["Score"] for i in scores if (i["Claan"] == "Earth Striders")
+            ]
             print(earth_scores)
             # Add metric for claan score
-            st.metric(label="Earth Striders", value=sum(earth_scores), delta=earth_scores[-1])
+            st.metric(
+                label="Earth Striders", value=sum(earth_scores), delta=earth_scores[-1]
+            )
 
         # Add content to second column
         with col2:
             # Add the claan image
             st.image(fire_img)
             # Get scores for the claan
-            fire_scores = [i['Score'] for i in scores if (i['Claan']=="Fire Dancers")]
+            fire_scores = [i["Score"] for i in scores if (i["Claan"] == "Fire Dancers")]
             # Add metric for claan score, using the sum of the scores, and the last entry as the delta
-            st.metric(label="Fire Dancers", value=sum(fire_scores), delta=fire_scores[-1])
+            st.metric(
+                label="Fire Dancers", value=sum(fire_scores), delta=fire_scores[-1]
+            )
 
         # Add content to third column
         with col3:
             # Add the claan image
             st.image(thunder_img)
             # Get scores for the claan
-            thunder_scores = [i['Score'] for i in scores if (i['Claan']=="Thunder Walkers")]
+            thunder_scores = [
+                i["Score"] for i in scores if (i["Claan"] == "Thunder Walkers")
+            ]
             # Add metric for claan score, using the sum of the scores, and the last entry as the delta
-            st.metric(label="Thunder Walkers", value=sum(thunder_scores), delta=thunder_scores[-1])
+            st.metric(
+                label="Thunder Walkers",
+                value=sum(thunder_scores),
+                delta=thunder_scores[-1],
+            )
 
         # Add content to last column
         with col4:
             # Add the claan image
             st.image(wave_img)
             # Get scores for the claan
-            wave_scores = [i['Score'] for i in scores if (i['Claan']=="Wave Riders")]
+            wave_scores = [i["Score"] for i in scores if (i["Claan"] == "Wave Riders")]
             # Add metric for claan score, using the sum of the scores, and the last entry as the delta
-            st.metric(label="Wave Riders", value=sum(wave_scores), delta=wave_scores[-1])
+            st.metric(
+                label="Wave Riders", value=sum(wave_scores), delta=wave_scores[-1]
+            )
 
         # Add spacer
         st.write("---")
@@ -116,15 +126,25 @@ def main():
     # Add section for changes
     with st.container():
         st.title("How it works!")
-        st.write("Claans and the Claan competition are here as a tool to encourage a healthy work life balance, and to promote practicing self care! Plus it is a great opportunity to socialise and cooperate with others in the company who aren't in your department and project teams!")
+        st.write(
+            "Claans and the Claan competition are here as a tool to encourage a healthy work life balance, and to promote practicing self care! Plus it is a great opportunity to socialise and cooperate with others in the company who aren't in your department and project teams!"
+        )
         st.subheader("Quests")
-        st.write("Each fortnight there will be 5 quests for each member member of the Claan to complete. The reward for each quest will be a dice, ranging from D4 to D10 points. It will be automatically rolled and the result added to your Claan's score.")
+        st.write(
+            "Each fortnight there will be 5 quests for each member member of the Claan to complete. The reward for each quest will be a dice, ranging from D4 to D10 points. It will be automatically rolled and the result added to your Claan's score."
+        )
         st.info("D4 - D10 Quests can now be completed daily!")
-        st.info("The D12 quest will always be a Claan Challenge, it is generally completed as a team and can only completed once per person")
+        st.info(
+            "The D12 quest will always be a Claan Challenge, it is generally completed as a team and can only completed once per person"
+        )
         st.subheader("Steps")
-        st.write("Step counting is back, and this time it is simple! If you complete 10,000 steps or more in a day, you can log it in the portal and claim yourself D4 points!")
+        st.write(
+            "Step counting is back, and this time it is simple! If you complete 10,000 steps or more in a day, you can log it in the portal and claim yourself D4 points!"
+        )
         st.subheader("Activities")
-        st.write("Activities are also back! Each week different activities will be incentivised but by default 45 minutes of indoor exercise will net you D6 points, 45 minutes of outdoor exercise will net you D8 points, and participating in a team sport will net you D10 points!")
+        st.write(
+            "Activities are also back! Each week different activities will be incentivised but by default 45 minutes of indoor exercise will net you D6 points, 45 minutes of outdoor exercise will net you D8 points, and participating in a team sport will net you D10 points!"
+        )
         st.info("All activities can be logged daily too!")
 
 
--- pages/Thunder_Walkers.py
+++ pages/Thunder_Walkers.py
@@ -7,7 +7,6 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 
 CLAAN = "Thunder Walkers"
 
@@ -21,6 +20,7 @@ claan_img = Image.open(f"{img_path}/Page_Images/Thunder-walkers-hex.png")
 
 def check_password():
     """Returns `True` if the user had the correct password."""
+
     def password_entered():
         """Checks whether a password entered by the user is correct."""
         if st.session_state["password"] == st.secrets["thunder_password"]:
@@ -49,18 +49,11 @@ def check_password():
         return True
 
 
-
 if "db" not in st.session_state:
-    # Get the secret mongo username and password
-    mongo_user = st.secrets["MONGO_USER"]
-    mongo_pass = st.secrets["MONGO_PASS"]
-    # Create the connection string
-    uri = f"mongodb+srv://{mongo_user}:{mongo_pass}@claanapp.l2vlfwo.mongodb.net/?retryWrites=true&w=majority"
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
+    client: MongoClient = MongoClient(**st.secrets["mongo"])
     # Send a ping to confirm a successful connection
     try:
-        client.admin.command('ping')
+        client.admin.command("ping")
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
         print(e)
@@ -78,8 +71,22 @@ if "settings" not in st.session_state:
 
 
 def get_data():
-    st.session_state.total_score = [i['Score'] for i in st.session_state.score_col.find({"Claan": CLAAN})]
-    scores = [i for i in st.session_state.score_col.find({"Date": {"$gte": datetime.datetime.strptime(st.session_state.settings['Fortnight Start Date'], ("%d/%m/%Y"))}, "Claan": CLAAN})]
+    st.session_state.total_score = [
+        i["Score"] for i in st.session_state.score_col.find({"Claan": CLAAN})
+    ]
+    scores = [
+        i
+        for i in st.session_state.score_col.find(
+            {
+                "Date": {
+                    "$gte": datetime.datetime.strptime(
+                        st.session_state.settings["Fortnight Start Date"], ("%d/%m/%Y")
+                    )
+                },
+                "Claan": CLAAN,
+            }
+        )
+    ]
     print(scores)
     # Create counters for the scores and activity counters
     fortnight_score = 0
@@ -88,19 +95,31 @@ def get_data():
 
     # Iterate over scores and increment relevant counters
     for i in scores:
-        fortnight_score += i['Score']
+        fortnight_score += i["Score"]
 
-        if "Type" in i and i['Type'] == "Quest":
+        if "Type" in i and i["Type"] == "Quest":
             fortnight_quests += 1
-        elif "Type" in i and i['Type'] == "Activity":
+        elif "Type" in i and i["Type"] == "Activity":
             fortnight_activities += 1
-    
+
     # Set session_state variables
     st.session_state.fortnight_score = fortnight_score
     st.session_state.fortnight_quests = fortnight_quests
     st.session_state.fortnight_activities = fortnight_activities
 
-    st.session_state.submissions = [i for i in st.session_state.col.find({"Date": {"$gte": datetime.datetime.strptime(st.session_state.settings['Fortnight Start Date'], ("%d/%m/%Y"))}}, { "_id": 0})]
+    st.session_state.submissions = [
+        i
+        for i in st.session_state.col.find(
+            {
+                "Date": {
+                    "$gte": datetime.datetime.strptime(
+                        st.session_state.settings["Fortnight Start Date"], ("%d/%m/%Y")
+                    )
+                }
+            },
+            {"_id": 0},
+        )
+    ]
 
 
 def submit_quest():
@@ -108,15 +127,17 @@ def submit_quest():
         st.toast("Please select your name!!")
 
     else:
-        roll = random.randint(1, st.session_state.settings["Quests"][st.session_state.quest])
-        
+        roll = random.randint(
+            1, st.session_state.settings["Quests"][st.session_state.quest]
+        )
+
         # Create submission for quest log
         submission = {
             "Name": st.session_state.qname,
             "Date": datetime.datetime.now(),
             "Quest/Activity": st.session_state.quest,
             "Dice step": st.session_state.settings["Quests"][st.session_state.quest],
-            "Roll": roll
+            "Roll": roll,
         }
 
         # Create submission for scores
@@ -124,22 +145,34 @@ def submit_quest():
             "Claan": CLAAN,
             "Score": roll,
             "Type": "Quest",
-            "Date": datetime.datetime.now()
+            "Date": datetime.datetime.now(),
         }
 
         # Get today's date at midnight (00:00:00)
-        today_start = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        today_start = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
         # Get tomorrow's date at midnight (00:00:00)
-        tomorrow_start = today_start + datetime.timedelta(days = 1)
+        tomorrow_start = today_start + datetime.timedelta(days=1)
 
         # Find documents where the creation time falls within today's range
         query = {}
 
-        temp_lookup = [i for i in st.session_state.col.find({"Date": {"$gte": today_start, "$lt": tomorrow_start}, "Name": st.session_state.qname, "Quest": st.session_state.quest}, { "_id": 0})]
+        temp_lookup = [
+            i
+            for i in st.session_state.col.find(
+                {
+                    "Date": {"$gte": today_start, "$lt": tomorrow_start},
+                    "Name": st.session_state.qname,
+                    "Quest": st.session_state.quest,
+                },
+                {"_id": 0},
+            )
+        ]
 
         # Attempt to upload response to DB
         try:
-            if len(temp_lookup) >= 1: 
+            if len(temp_lookup) >= 1:
                 st.toast("You have already completed that quest today!")
             else:
                 st.session_state.col.insert_one(submission)
@@ -150,20 +183,25 @@ def submit_quest():
             print(e)
             st.toast("Submission failed!")
 
+
 def submit_activity():
     if st.session_state.aname == "Please select your name":
         st.toast("Please select your name!!")
 
     else:
-        roll = random.randint(1, st.session_state.settings["Activities"][st.session_state.activity])
+        roll = random.randint(
+            1, st.session_state.settings["Activities"][st.session_state.activity]
+        )
 
         # Create submission for quest log
         submission = {
             "Name": st.session_state.aname,
             "Date": datetime.datetime.now(),
             "Quest/Activity": st.session_state.activity,
-            "Dice step": st.session_state.settings["Activities"][st.session_state.activity],
-            "Roll": roll
+            "Dice step": st.session_state.settings["Activities"][
+                st.session_state.activity
+            ],
+            "Roll": roll,
         }
 
         # Create submission for scores
@@ -171,22 +209,34 @@ def submit_activity():
             "Claan": CLAAN,
             "Score": roll,
             "Type": "Activity",
-            "Date": datetime.datetime.now()
+            "Date": datetime.datetime.now(),
         }
 
         # Get today's date at midnight (00:00:00)
-        today_start = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        today_start = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
         # Get tomorrow's date at midnight (00:00:00)
-        tomorrow_start = today_start + datetime.timedelta(days = 1)
+        tomorrow_start = today_start + datetime.timedelta(days=1)
 
         # Find documents where the creation time falls within today's range
         query = {}
 
-        temp_lookup = [i for i in st.session_state.col.find({"Date": {"$gte": today_start, "$lt": tomorrow_start}, "Name": st.session_state.aname, "Activity": st.session_state.activity}, { "_id": 0})]
+        temp_lookup = [
+            i
+            for i in st.session_state.col.find(
+                {
+                    "Date": {"$gte": today_start, "$lt": tomorrow_start},
+                    "Name": st.session_state.aname,
+                    "Activity": st.session_state.activity,
+                },
+                {"_id": 0},
+            )
+        ]
 
         # Attempt to upload response to DB
         try:
-            if len(temp_lookup) >= 1: 
+            if len(temp_lookup) >= 1:
                 st.toast("You have already completed that activity today!")
             else:
                 st.session_state.col.insert_one(submission)
@@ -207,25 +257,29 @@ def main():
         # Header section
         with st.container():
             # Create columns
-            head_l, head_r = st.columns((3,1))
+            head_l, head_r = st.columns((3, 1))
 
             with head_l:
                 # Add a subheader
                 st.subheader("Advancing Analytics")
                 # Add a title
                 st.title(CLAAN)
-                st.write(f"Welcome to the {CLAAN} Claan area! Here you can log quests, activities and steps!")
+                st.write(
+                    f"Welcome to the {CLAAN} Claan area! Here you can log quests, activities and steps!"
+                )
 
                 st.subheader("Fortnight Breakdown!")
-                col1, col2, col3, col4 = st.columns((1,1,1,1))
-                with col1: 
+                col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
+                with col1:
                     st.metric("Overall Score!", sum(st.session_state.total_score))
                 with col2:
                     st.metric("Score", st.session_state.fortnight_score)
                 with col3:
                     st.metric("Quest Completed", st.session_state.fortnight_quests)
                 with col4:
-                    st.metric("Activities Completed", st.session_state.fortnight_activities)
+                    st.metric(
+                        "Activities Completed", st.session_state.fortnight_activities
+                    )
             with head_r:
                 # Add logo
                 st.image(claan_img)
@@ -233,22 +287,26 @@ def main():
             st.write("---")
 
         with st.container():
-            # Create columns for logging the two 
-            log_l, log_r = st.columns((1,1))
+            # Create columns for logging the two
+            log_l, log_r = st.columns((1, 1))
 
             with log_l:
                 # Add heading
                 st.header("Complete a quest")
 
                 # Name
-                st.session_state.qname = st.selectbox('Please tell me who you are!',
-                                    tuple(["Please select your name"] + st.session_state.settings[CLAAN]), key=0
-                                )
+                st.session_state.qname = st.selectbox(
+                    "Please tell me who you are!",
+                    tuple(
+                        ["Please select your name"] + st.session_state.settings[CLAAN]
+                    ),
+                    key=0,
+                )
 
                 # Acitivity/challenge
-                st.session_state.quest = st.radio('What quest did you complete?',
-                                st.session_state.settings["Quests"]
-                            )
+                st.session_state.quest = st.radio(
+                    "What quest did you complete?", st.session_state.settings["Quests"]
+                )
                 st.button("Submit Quest.", on_click=submit_quest)
 
             with log_r:
@@ -256,17 +314,24 @@ def main():
                 st.header("Log steps or activities")
 
                 # Name
-                st.session_state.aname = st.selectbox('Please tell me who you are!',
-                                    tuple(["Please select your name"] + st.session_state.settings[CLAAN]), key=1
-                                )
+                st.session_state.aname = st.selectbox(
+                    "Please tell me who you are!",
+                    tuple(
+                        ["Please select your name"] + st.session_state.settings[CLAAN]
+                    ),
+                    key=1,
+                )
 
                 # Acitivity/challenge
-                st.session_state.activity = st.radio('What quest did you complete?',
-                                st.session_state.settings["Activities"]
-                            )
+                st.session_state.activity = st.radio(
+                    "What quest did you complete?",
+                    st.session_state.settings["Activities"],
+                )
                 st.button("Log activity.", on_click=submit_activity)
 
-            refugee_week_bingo = pathlib.Path("./pages/Page_Images/refugee_week_bingo.jpg")
+            refugee_week_bingo = pathlib.Path(
+                "./pages/Page_Images/refugee_week_bingo.jpg"
+            )
             if refugee_week_bingo.exists():
                 st.image(str(refugee_week_bingo))
 
@@ -275,11 +340,13 @@ def main():
         with st.container():
             # Add some text
             st.header("Detailed View")
-            st.write("Press the button below to see the details of what people have been up to! (It is hidden by default as it can be a little slow to load!)")
+            st.write(
+                "Press the button below to see the details of what people have been up to! (It is hidden by default as it can be a little slow to load!)"
+            )
             # Add button to display claan activity when pressed
-            if st.button('View'):
+            if st.button("View"):
                 st.table(st.session_state.submissions)
 
 
 if __name__ == "__main__":
-     main()
+    main()
